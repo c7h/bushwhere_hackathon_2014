fd19d0212bc53e47a7a8d03cd48cace7434f8cc7
c7h <christoph@gerneth.info>
2014-12-06 15:19:50 -0500

    fixed admin and settings

--- backend/bushwhere/api/admin.py
+++ backend/bushwhere/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Place, User, 
+from models import Place, User, Hint
 
 admin.site.register(Place)
 admin.site.register(User)
--- backend/bushwhere/bushwhere/settings.py
+++ backend/bushwhere/bushwhere/settings.py
@@ -123,7 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    'rest_framework'
+    'rest_framework',
+    'api'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
