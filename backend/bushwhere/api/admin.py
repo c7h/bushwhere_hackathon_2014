779861abae31fed2fd697bf62f18a69bdaec2af3
c7h <christoph@gerneth.info>
2014-12-06 15:01:35 -0500

    fixed import error

--- backend/bushwhere/api/admin.py
+++ backend/bushwhere/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from polls.models import Place, User, Hint
+from .models import Place, User, 
 
-admin.site.register(Places)
+admin.site.register(Place)
 admin.site.register(User)
 admin.site.register(Hint)
