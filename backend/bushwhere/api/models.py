53e4a014c0442fceb8bb9b327a80b953b44ea9dd
c7h <christoph@gerneth.info>
2014-12-06 13:42:14 -0500

    changed settings

--- backend/bushwhere/api/models.py
+++ backend/bushwhere/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Places(models.Model):
+	name = models.CharField(max_length=100)
+	lat = models.FloatField()
+	lon = models.FloatField()
+
+	# for additional urls (Wikipedia...)
+	url = models.URLfield()
+
+class User(model.Model):
+	name = models.CharField(max_length=100)
+
+class Hint(models.Model):
+	text = models.TextField()
+	place = models.ForeignKey(Places)
+		
