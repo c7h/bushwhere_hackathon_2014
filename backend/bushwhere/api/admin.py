cab7ee93e6e2e2c0de43e2c497b19cbc455fa7f8
c7h <christoph@gerneth.info>
2014-12-11 23:52:11 -0500

    changed representation of tables in admin interface

--- backend/bushwhere/api/admin.py
+++ backend/bushwhere/api/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from models import Place, Player, Hint, Visit
+from models import Place, Player, Hint, Visit, Mission
 
-admin.site.register(Place)
-admin.site.register(Player)
-admin.site.register(Hint)
-admin.site.register(Visit)
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+	list_display = ('player', 'place')
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+
+@admin.register(Hint)
+class HintAdmin(admin.ModelAdmin):
+	list_display = ('id', 'place')
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+	list_display = ('name', 'lat', 'lon')
