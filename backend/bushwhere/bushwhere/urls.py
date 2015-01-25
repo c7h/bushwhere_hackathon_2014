20220f405aea225dc23770480acdf501384f9081
Christoph Gerneth <login@gerneth.info>
2015-01-25 18:48:25 -0500

    changed urlpattern for api - now reachable under /api

--- backend/bushwhere/bushwhere/urls.py
+++ backend/bushwhere/bushwhere/urls.py
@@ -17,8 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-    url(r'^', include('api.urls')),
+    url(r'^api/', include('api.urls')),
 
 )
 
